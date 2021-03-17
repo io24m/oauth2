8c9f2c873f38876cebc76a4fe3ba1831951b1af4
io24m <io24m@outlook.com>
2021-03-17 17:03:40 +0800

    .

--- platform/src/main/java/com/github/io24m/oauth2/platform/PlatformApplication.java
+++ platform/src/main/java/com/github/io24m/oauth2/platform/PlatformApplication.java
@@ -2,13 +2,10 @@ package com.github.io24m.oauth2.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
-@EnableOAuth2Sso
 public class PlatformApplication {
     @Bean
     public RequestContextListener requestContextListener() {
--- platform/src/main/java/com/github/io24m/oauth2/platform/config/WebSecurityConfig.java
+++ platform/src/main/java/com/github/io24m/oauth2/platform/config/WebSecurityConfig.java
@@ -1,39 +1,34 @@
-//package com.github.io24m.oauth2.platform.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.annotation.Order;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-////@Order(1)
-//@Configuration
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-////        http.authorizeRequests().anyRequest().authenticated().and().csrf().disable();
-//
-////        http.authorizeRequests()
-////                .antMatchers("/", "/index").authenticated()
-////                //region 权限码
-////                //.antMatchers("/sys/user/**").hasAuthority("sys-user")
-////                .anyRequest().authenticated()//.hasAuthority("admin-service-a")
-//////                .requestMatchers(getRequestMatcher("/sys/notication/**")).hasAuthority("sys-notication")
-//////                .requestMatchers(getRequestMatcher("/sys/hospitalgp/**")).hasAuthority("sys-hospitalgp")
-//////                //endregion
-////                //.antMatchers("/**/**").authenticated()
-////                //.anyRequest().hasAuthority("not_allow")//这一行强制添加，必须加
-////                //.and().logout().permitAll()
-////                //关闭跨站请求防护
-////                .and().csrf().disable();
-////        //.headers().frameOptions().sameOrigin();
-//        org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2SsoDefaultConfiguration
-//        http.authorizeRequests()
-//                .antMatchers("/", "/index").authenticated()
-//                .antMatchers("/login", "/logout").permitAll()
-//                .antMatchers("/**").authenticated()
-////                .anyRequest().hasAuthority("not_allow")
-//                .and().logout().permitAll()
-//                .and().csrf().disable();
-//    }
-//}
+package com.github.io24m.oauth2.platform.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableOAuth2Sso
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${io24m.logout:/logout}")
+    private String logout;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //http.authorizeRequests().anyRequest().authenticated().and().csrf().disable();
+
+        http.authorizeRequests()
+                //region 权限码
+//                .requestMatchers(getRequestMatcher("/sys/notication/**")).hasAuthority("sys-notication")
+                .antMatchers("/**/**").authenticated()
+                .anyRequest().hasAuthority("not_allow")//这一行强制添加，必须加
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true).permitAll()
+                //关闭跨站请求防护
+                .and().csrf().disable()
+                .headers().frameOptions().sameOrigin();
+    }
+}
